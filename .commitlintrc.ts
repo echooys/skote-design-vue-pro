9a5ffa083ac80bb0b5c827d838e88820aa39419b
lance <lance.li@yimlink.com>
2022-05-07 13:40:52 +0800

    chore(config): :wrench: commitlint rules

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -12,15 +12,16 @@ export default {
       2,
       'always',
       [
-        'feat', // 新功能 feature
-        'fix', // 修复 bug
-        'docs', // 文档注释
+        'test', // 增加测试
         'style', // 代码格式(不影响代码运行的变动)
+        'revert', // 回退
         'refactor', // 重构(既不增加新功能，也不是修复bug)
         'perf', // 性能优化
-        'test', // 增加测试
+        'fix', // 修复 bug
+        'feat', // 新功能 feature
+        'docs', // 文档注释
+        'ci', // 自动化
         'chore', // 构建过程或辅助工具的变动
-        'revert', // 回退
         'build', // 打包
       ],
     ],
